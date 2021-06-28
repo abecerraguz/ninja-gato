8e6537b84425edcecf8b7c4f9810d852c75e3e87
abecerraguz <alejandro.becerra@publimetro.cl>
2021-06-28 11:06:23 -0400

    Se guardan los cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,33 @@
 $(document).ready(function(){
 
+    $('img').click(function() {
+        let cat = $(this).attr('data-alt-src');
+        $(this).attr('src',`assets/img/${cat}`);
+    });
+    // randomImg();
 
-    randomImg();
-
-    function randomImg(){
+    // function randomImg(){
         
-        let arr = [1,2,3,4,5,6,7,8]
+    //     let arr = [1,2,3,4,5,6,7,8]
        
-        let newArr =[]
+    //     let newArr =[]
 
-        for(let i = 0; i<=arr.length;i++){
-            var num = Math.round(Math.random() * (8 - 1) + 1)
-            newArr.push(`assets/img/imagen-${num}.jpeg`);
-            $('#picture').append( `<li><img src="assets/img/imagen-${num}.jpeg"/></li>` );   
-        }
-        return newArr;
-    }
+    //     for(let i = 0; i<=arr.length;i++){
+    //         var num = Math.round(Math.random() * (8 - 1) + 1)
+    //         newArr.push(`assets/img/imagen-${num}.jpeg`);
+    //         $('#picture').append( `<li><img src="assets/img/imagen-${num}.jpeg"/></li>` );   
+    //     }
+    //     return newArr;
+    // }
 
-    $('li').click(function(){
-        $(this).hide();
-    })
+    // $('li').click(function(){
+    //     $(this).hide();
+    // })
 
-    $('.button').click(function(e){
-        e.preventDefault();
-        randomImg();
-    })
+    // $('.button').click(function(e){
+    //     e.preventDefault();
+    //     randomImg();
+    // })
     
 
 
